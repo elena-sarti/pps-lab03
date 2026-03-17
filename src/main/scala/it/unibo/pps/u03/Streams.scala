44960709ec5d709576e959c1cec33c021f57bd29
Elena Sarti <elena.sarti8@studio.unibo.it>
2026-03-17 14:22:13 +0100

    Task3 ex6

--- src/main/scala/it/unibo/pps/u03/Streams.scala
+++ src/main/scala/it/unibo/pps/u03/Streams.scala
@@ -37,6 +37,11 @@ object Streams extends App :
     def iterate[A](init: => A)(next: A => A): Stream[A] =
       cons(init, iterate(next(init))(next))
 
+    def takeWhile[A](stream: Stream[A])(pred: A => Boolean): Stream[A] = stream match
+      case Cons(head, tail) if pred(head()) => cons(head(), takeWhile(tail())(pred))
+      case _ => Empty()
+
+
   end Stream
 
 @main def tryStreams =
@@ -46,7 +51,10 @@ object Streams extends App :
   val str2 = Stream.map(str1)(_ + 1) // {1,2,3,4,..}
   val str3 = Stream.filter(str2)(x => (x < 3 || x > 20)) // {1,2,21,22,..}
   val str4 = Stream.take(str3)(10) // {1,2,21,22,..,28}
-  println(Stream.toList(str4)) // [1,2,21,22,..,28]
+  val str5 = Stream.takeWhile(Stream.iterate(0)(_ + 1))(_ < 5)
+
+  println(Stream.toList(str5)) //  Cons (0 , Cons (1 , Cons (2 , Cons (3 , Cons (4 , Nil ())))))
+
 
   lazy val corec: Stream[Int] = Stream.cons(1, corec) // {1,1,1,..}
-  println(Stream.toList(Stream.take(corec)(10))) // [1,1,..,1]
+  //println(Stream.toList(Stream.take(corec)(10))) // [1,1,..,1]
